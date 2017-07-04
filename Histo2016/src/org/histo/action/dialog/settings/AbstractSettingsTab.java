1bd00c8914bd0f43fd8d534b768ee51a9d9df0d0
glatza <andreas.glatz@uniklinik-freiburg.de>
2017-07-04 17:05:02 +0200

    settings update

---
+++ Histo2016/src/org/histo/action/dialog/settings/AbstractSettingsTab.java
@@ -0,0 +1,9 @@
+package org.histo.action.dialog.settings;
+
+import org.apache.log4j.Logger;
+
+public class AbstractSettingsTab {
+
+	protected static Logger logger = Logger.getLogger("org.histo");
+
+}
